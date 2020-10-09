6e3248aea319a44a3aa462726cf45410e06469f2
loganfillo <logan.fillo@gmail.com>
2020-10-08 21:12:18 -0700

    added OpenCV dependencies to vision utils and gate

--- src/triton_gate/src/gate_detector.cpp
+++ src/triton_gate/src/gate_detector.cpp
@@ -1,8 +1,11 @@
-#include "triton_vision_utils/vision_utils.hpp"
 #include <iostream>
+#include <opencv2/core/mat.hpp>
+
+#include "triton_vision_utils/vision_utils.hpp"
 
 int main()
 {
+  cv::Mat matrix;
   int z = vision_utils::add(5,3);
   std::cout<<z<<std::endl;
 
--- src/triton_vision_utils/src/vision_utils.cpp
+++ src/triton_vision_utils/src/vision_utils.cpp
@@ -1,10 +1,12 @@
 #include "triton_vision_utils/vision_utils.hpp"
+#include <opencv2/core/mat.hpp>
 
 namespace vision_utils
 {
 
 int add(int x, int y)
-{
+{   
+    cv::Mat matrix;
     return x + y;
 }
     
