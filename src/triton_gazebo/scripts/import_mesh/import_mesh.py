ddc7aed54694464bd8ef3c86611046d816b1a828
Jake <jcronin97@live.ca>
2020-10-25 17:10:56 -0700

    Added solution to merge sdf files

--- src/triton_gazebo/scripts/import_mesh/import_mesh.py
+++ src/triton_gazebo/scripts/import_mesh/import_mesh.py
@@ -1,18 +1,21 @@
 import os
 import subprocess
 import sys
+import shutil
 import json
 import argparse
 import re
 
+from xml_combiner import XMLCombiner
+
 # Needs to change
 REPO_ROOT = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).decode('utf-8')[:-1]
-
 
 def convert_mesh(model_path, model):
     """
     Invoke Blender command line tooks with script containing commands to import and export
     """
+    print("Blender Version:\n")
     pre_check = subprocess.check_output(['blender', '--version']).decode('utf-8') 
 
     if 'Blender 2.8' not in pre_check:
@@ -21,8 +24,10 @@ def convert_mesh(model_path, model):
 
     convert_mesh_path = os.path.join(REPO_ROOT, 'src/triton_gazebo/scripts/import_mesh/convert_mesh.py')
     run_cmd = ['blender', '--background', '--python', convert_mesh_path, '--', model_path, model]
-
+ 
+    # Need to catch errors here
     subprocess.run(run_cmd)
+    print('Model mesh expoerted successfully\n')
 
 
 def convert_urdf(model_path, model_name):
@@ -32,6 +37,8 @@ def convert_urdf(model_path, model_name):
     sdf_file = os.path.join(model_path, 'model.sdf')
     urdf_file = os.path.join(model_path, 'urdf', (model_name + '.urdf'))
 
+    print('Converting URDF to SDF format...\n')
+
     if not os.path.exists(urdf_file): 
         print('ERROR: urdf file, %s not found, please ensure directory was generated correctly, exiting' % urdf_file)
         sys.exit(1)
@@ -39,18 +46,29 @@ def convert_urdf(model_path, model_name):
     run_cmd = ['gz', 'sdf', '-p' , urdf_file, '>', sdf_file]
     sdf_data = subprocess.check_output(run_cmd).decode('utf-8')[:-1]
 
-    return sdf_data.splitlines()
+    print('SDF converted successfully\n')
+
+    return sdf_data.replace('.STL', '.dae').splitlines()
 
 
-def dict_to_sdf(parameters, sdf_fmt=[], depth=1):
+def dict_to_sdf(model_name, parameters, sdf_fmt=[], depth=1):
     """
-    converts a dictionary to a section of xml code
+    converts a dictionary to a section of xml code and applies model name to model, link, visual and collision 
     """
     for key, data in parameters.items():
-        indent = '\t'.join(['']*(depth+1))
+        indent = '  '.join(['']*(depth+1))
         if isinstance(data, dict):
-            sdf_fmt.append(indent + '<'+key+'>')
-            dict_to_sdf(data, sdf_fmt, depth+1)
+            if key == 'model' or key == 'link':
+                sdf_fmt.append(indent + '<'+key+' name=\'%s\'>'%model_name)
+            elif key == 'visual':
+                sdf_fmt.append(indent + '<'+key+' name=\'%s_visual\'>'%model_name)
+            elif key == 'collision':
+                sdf_fmt.append(indent + '<'+key+' name=\'%s_collision\'>'%model_name)
+            elif key == 'shader':
+                sdf_fmt.append(indent + '<'+key+' type=\'pixel\'>')
+            else:
+                sdf_fmt.append(indent + '<'+key+'>')
+            dict_to_sdf(model_name, data, sdf_fmt, depth+1)
             sdf_fmt.append(indent + '</'+key+'>')
         elif isinstance(data, list):
             sdf_fmt.append(indent + '<'+key+'>'+' '.join(map(str, data))+'</'+key+'>')
@@ -67,7 +85,9 @@ def main():
     parser.add_argument('--params', '-p', dest='parameters', required=False,
                         default='import_mesh/config/default_parameters.json',
                         help='json file containing model parameters')
-
+    parser.add_argument('--clean-up', '-cu', dest='cleanup', required=False,
+                        type=bool, default=False,
+                        help='remove old files after successful conversion')
     args = parser.parse_args()
 
     model_path = os.path.join(REPO_ROOT, 'src/triton_gazebo/models', args.model)
@@ -78,11 +98,56 @@ def main():
 
     convert_mesh(model_path, args.model)
 
-    sdf_data = convert_urdf(model_path, args.model)
+    sdf_data = ['<?xml version=\'1.0\'?>'] + convert_urdf(model_path, args.model)
 
-    with open(os.path.join(REPO_ROOT, 'src/triton_gazebo/scripts/', args.parameters)) as param_file:
+    with open(os.path.join(REPO_ROOT, 'src/triton_gazebo/scripts/', args.parameters), 'r') as param_file:
         params = json.load(param_file)
 
+    params_sdf = ['<?xml version=\'1.0\'?>', '<sdf version=\'1.7\'>']
+    params_sdf += dict_to_sdf(args.model, params)
+    params_sdf += ['</sdf>']
+
+    if not os.path.exists(os.path.join(model_path, 'temp')):
+        os.makedirs(os.path.join(model_path, 'temp'))
+
+    with open(os.path.join(model_path, 'temp', 'params.sdf'), 'w') as sdf_file:
+        sdf_file.write('\n'.join(params_sdf))
+    with open(os.path.join(model_path, 'temp', 'model_org.sdf'), 'w') as sdf_file:
+        sdf_file.write('\n'.join(sdf_data))
+
+
+    result = '<?xml version=\'1.0\'?>\n' + XMLCombiner((os.path.join(model_path, 'temp', 'model_org.sdf'),
+                                                        os.path.join(model_path, 'temp', 'params.sdf'))
+                                                        ).combine().decode('utf-8')[:-1]
+
+    with open(os.path.join(model_path, 'model.sdf'), 'w') as sdf_file:
+        sdf_file.write(result)
+
+    model_config = [
+        '<?xml version=\'1.0\'?>',
+        '',
+        '<model>',
+        '  <name>%s</name>' % args.model,
+        '  <version>1.0</version>',
+        '  <sdf version=\'1.7\'>model.sdf</sdf>',
+        '',
+        '  <author>',
+        '    <name></name>',
+        '    <email></email>',
+        '  </author>',
+        '',
+        '  <description>',
+        '  </description>',
+        '</model>'
+        ]
+
+    with open(os.path.join(model_path, 'model.config'), 'w') as config_file:
+        config_file.write('\n'.join(model_config))
+
+    if args.cleanup:
+        shutil.rmtree(os.path.join(model_path, 'temp'))
+        shutil.rmtree(os.path.join(model_path, 'urdf'))
+        os.remove(os.path.join(model_path, 'meshes', args.model+'.STL'))
 
 
 if __name__ == '__main__': 
