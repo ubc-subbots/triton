6f92ccb84c2607125cf70d3f1a363f086c2b28e1
loganfillo <logan.fillo@gmail.com>
2020-10-29 15:47:37 -0700

    output thrust implemented

--- src/triton_controls/include/triton_controls/thrust_allocator.hpp
+++ src/triton_controls/include/triton_controls/thrust_allocator.hpp
@@ -1,9 +1,14 @@
 #ifndef TRITON_CONTROL__THRUST_ALLOCATOR
 #define TRITON_CONTROL__THRUST_ALLOCATOR
 
+#include <math.h>
+#include <utility>
+#include <vector>
+
+#include <opencv2/core.hpp>
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/wrench.hpp"
-#include "std_msgs/msg/float32_multi_array.hpp"
+#include "std_msgs/msg/float64_multi_array.hpp"
 
 namespace triton_controls
 {      
@@ -13,7 +18,6 @@ namespace triton_controls
 
     public:
 
-
         /** Constructor
          * 
          * TODO: Better explanation
@@ -22,9 +26,7 @@ namespace triton_controls
          */
         explicit ThrustAllocator(const rclcpp::NodeOptions & options);
 
-
     private:
-
 
         /** Wrench message callback
          * 
@@ -34,10 +36,29 @@ namespace triton_controls
          */
         void wrenchCallback(const geometry_msgs::msg::Wrench::SharedPtr msg) const;
 
-        rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr forces_pub_;  
-        rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr signals_pub_;  
+        /** Cosine and sine in degrees
+         * 
+         * @param deg angle in degrees
+         */
+        inline float cosd(float deg) { return cos( deg*M_PI/180.0); }
+        inline float sind(float deg) { return cos( deg*M_PI/180.0); }
+
+        /** Helper for getting magnitude/direction of thrust from x/y parts
+         * 
+         * @param x the x-component of the thrust
+         * @param y the y-component of the thrust
+         */
+        inline float getThrust(float x, float y) const{ 
+            return copysign(1.0,x)*sqrt(pow(x,2) + pow(y,2));
+        }
+
+        rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr forces_pub_;  
+        rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr signals_pub_;  
 
         rclcpp::Subscription<geometry_msgs::msg::Wrench>::SharedPtr forces_sub_; 
+
+        cv::Mat alloc;
+
 
     };
     
--- src/triton_controls/src/thrust_allocator.cpp
+++ src/triton_controls/src/thrust_allocator.cpp
@@ -5,26 +5,82 @@ namespace triton_controls
 {
 
 
-ThrustAllocator::ThrustAllocator(const rclcpp::NodeOptions & options)
-: Node("thrust_allocator", options) 
-{
-    forces_pub_ = this->create_publisher<std_msgs::msg::Float32MultiArray>(
-      "output_forces", 10);
+  ThrustAllocator::ThrustAllocator(const rclcpp::NodeOptions & options)
+  : Node("thrust_allocator", options) 
+  {
 
-    signals_pub_ = this->create_publisher<std_msgs::msg::Float32MultiArray>(
-      "signals", 10);
+      double alpha = 45.0;
+      std::pair<double, double> l1(10.833*cosd(52.704), 10.833*sind(52.704));
+      std::pair<double, double> l2(11.238*cosd(54.261), 11.238*sind(54.261));
+      std::pair<double, double> l3(11.466*cosd(48.73), 11.466*sind(48.73));
+      std::pair<double, double> l4(11.85*cosd(50.34), 11.85*sind(50.34));
+      double l5 = 8.744;
+      double l6 = 8.239;
 
-    forces_sub_ = this->create_subscription<geometry_msgs::msg::Wrench>(
-      "/triton/controls/input_forces", 10, std::bind(&ThrustAllocator::wrenchCallback, this, _1));
+      double alloc_arr[10][10] = {
+          {-1, 0, -1, 0, 1, 0, 1, 0, 0, 0},
+          {0, -1, 0, 1, 0, -1, 0 ,1, 0, 0},
+          {0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+          {0, 0, 0, 0, 0, 0, 0, 0, -l5, l6},
+          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+          {-l1.first,-l1.second,l2.first,l2.second,
+            l3.first,l3.second,-l4.first,-l4.second,0,0},
+          {1/cosd(alpha), -1/sind(alpha), 0, 0, 0, 0, 0, 0, 0, 0},
+          {0, 0, 1/cosd(alpha), -1/sind(alpha), 0, 0, 0, 0, 0, 0},
+          {0, 0, 0, 0, 1/cosd(alpha), -1/sind(alpha), 0, 0, 0, 0},
+          {0, 0, 0, 0, 0, 0, 1/cosd(alpha), -1/sind(alpha), 0, 0}
+      };
 
-    RCLCPP_INFO(this->get_logger(), "Thrust Allocator succesfully started!");
-}
+      cv::Mat alloc_mat (10,10, CV_64F);
+      for (int i = 0; i < 10; i++)
+        for (int j = 0; j < 10; j++)
+          alloc_mat.at<double>(i,j) = alloc_arr[i][j];
+
+      cv::invert(alloc_mat, alloc, cv::DECOMP_SVD);
+
+      forces_pub_ = this->create_publisher<std_msgs::msg::Float64MultiArray>(
+        "output_forces", 10);
+
+      signals_pub_ = this->create_publisher<std_msgs::msg::Float64MultiArray>(
+        "signals", 10);
+
+      forces_sub_ = this->create_subscription<geometry_msgs::msg::Wrench>(
+        "/triton/controls/input_forces", 10, std::bind(&ThrustAllocator::wrenchCallback, this, _1));
+
+      RCLCPP_INFO(this->get_logger(), "Thrust Allocator succesfully started!");
+  }
 
 
-void ThrustAllocator::wrenchCallback(const geometry_msgs::msg::Wrench::SharedPtr msg) const
-{
+  void ThrustAllocator::wrenchCallback(const geometry_msgs::msg::Wrench::SharedPtr msg) const
+  {
+      double tau_arr[10] = {
+        msg->force.x, msg->force.y, msg->force.z,
+        msg->torque.x, msg->torque.y, msg->torque.z,
+        0,0,0,0
+      };
 
-}
+      cv::Mat tau_mat (10, 1, CV_64F);
+      for (int i = 0; i < 10; i++)
+        tau_mat.at<double>(i, 0) = tau_arr[i];
+
+      cv::Mat u_mat =  alloc*tau_mat;
+
+      std::vector<double> u;
+      for (int i = 0; i < 10; i++)
+        u.push_back(u_mat.at<double>(i,0));
+
+      double thrust_arr[6] = {
+        getThrust(u[0], u[1]), getThrust(u[2], u[3]),
+        getThrust(u[4], u[5]), getThrust(u[6], u[7]),
+        u[8], u[9]
+      };
+      std::vector<double> thrust (
+        thrust_arr, thrust_arr + sizeof(thrust_arr)/sizeof(thrust_arr[0]));
+
+      auto forces_msg = std_msgs::msg::Float64MultiArray();
+      forces_msg.data = thrust;
+      forces_pub_->publish(forces_msg);
+  }
     
 } // namespace triton_controls
 
