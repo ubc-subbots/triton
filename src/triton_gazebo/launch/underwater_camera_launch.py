0ffe19c291146e9c08282e8bc5f6c079acff00bd
Kevin Huang <kevinh42@student.ubc.ca>
2020-11-15 14:24:59 -0800

    Kevin/issue33 (#44)
    * added underwater camera node
    
    * added image buffers
    
    * implemented synching using message filters
    
    * added underwater camera as component
    
    * Added basic algorithm for underwater synthesis
    
    * Fixed colour scaling
    
    * Added config for launching underwater camera
    
    * Modified config
    
    * Turned on -O3 flag for fast Eigen
    
    * Fixed attenuation and switched to simpson's rule integration
    
    * Updated README
    
    Co-authored-by: loganfillo <logan.fillo@gmail.com>
    Co-authored-by: Kevin Huang <kevin42@alumni.ubc.ca>
    Co-authored-by: loganfillo <46413386+loganfillo@users.noreply.github.com>

---
+++ src/triton_gazebo/launch/underwater_camera_launch.py
@@ -0,0 +1,41 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import yaml
+
+def generate_launch_description():
+
+    ld = LaunchDescription()
+
+    config = os.path.join(
+        get_package_share_directory('triton_gazebo'),
+        'config',
+        'underwater_camera.yaml'
+    )
+
+    params = yaml.load(open(config,"r"), Loader=yaml.FullLoader)
+
+    camera_type = "SONY_IMX322"
+    water_type = "I"
+    irradiance_type = "ASTM_G173_03"
+
+    underwater_camera = Node(
+        name='underwater_camera',
+        namespace='/triton/drivers/front_camera',
+        package='triton_gazebo',
+        executable='underwater_camera',
+        output='screen',
+        parameters=[
+            {"rho": params["rho"]["default"]},
+            {"irradiance_transmission": params["irradiance_transmission"][water_type]},
+            {"spectral_sensitivity_blue": params["spectral_sensitivity"][camera_type][0]},
+            {"spectral_sensitivity_red": params["spectral_sensitivity"][camera_type][1]},
+            {"spectral_sensitivity_green": params["spectral_sensitivity"][camera_type][2]},
+            {"illumination_irradiance": params["illumination_irradiance"][irradiance_type]}
+        ]
+    )
+
+    ld.add_action(underwater_camera)
+
+    return ld
