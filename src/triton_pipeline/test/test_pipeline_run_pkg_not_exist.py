f7514bed4d71646c794745760d5bf59fc1e25628
loganfillo <logan.fillo@gmail.com>
2020-11-19 20:37:54 -0800

    seperated run tests, catch rclcpp err in thrustall

--- src/triton_pipeline/test/test_pipeline_run_pkg_not_exist.py
+++ src/triton_pipeline/test/test_pipeline_run_pkg_not_exist.py
@@ -81,79 +81,9 @@ class TestPipelineRun(unittest.TestCase):
         self.configure_client.destroy()
         self.run_client.destroy()
         self.node.destroy_node()
-        # kill zombie node (no idea why the zombie node exists...)
-        subprocess.call(['pkill ros'], shell=True, stdout=subprocess.PIPE)
 
 
-    def test_A_run_no_configure(self, pipeline_manager, proc_info, proc_output):
-        # force first test with 'A' (because zombie nodes exist, need to run in this order...)
-        goal_msg = RunPipeline.Goal()
-        goal_msg.input = 0
-        future = self.run_client.send_goal_async(goal_msg)
-
-        future_count = 0
-        result = None
-        while rclpy.ok():
-            rclpy.spin_once(self.node)
-            if future.done():
-                future_count += 1
-                if future_count == 1:
-                    future = future.result().get_result_async()
-                if future_count == 2:
-                    result = future.result().result
-                    break
-        if result.output == 1:
-            proc_output.assertWaitFor(
-                expected_output='Pipeline is not configured')
-        else:
-            self.fail('Should abort due to unconfigured pipeline')
-
-
-    def test_B_run_no_comp_not_valid(self, pipeline_manager, proc_info, proc_output):
-        # force second test with 'B'
-        req = ConfigurePipeline.Request()
-        pipeline_type = PipelineType()
-        test_type = 'example'
-        pipeline_type.type = test_type
-        req.pipeline_type = pipeline_type
-        test_file_name = 'test_run_comp_not_valid'
-        req.config_file_name = test_file_name
-        future = self.configure_client.call_async(req)
-
-        while rclpy.ok():
-            rclpy.spin_once(self.node)
-            if future.done():
-                break
-
-        goal_msg = RunPipeline.Goal()
-        goal_msg.input = 0
-        future = self.run_client.send_goal_async(goal_msg)
-
-        future_count = 0
-        result = None
-        while rclpy.ok():
-            rclpy.spin_once(self.node)
-            if future.done():
-                future_count += 1
-                if future_count == 1:
-                    future = future.result().get_result_async()
-                if future_count == 2:
-                    result = future.result().result
-                    break
-        if result.output == 1:
-            component = 'example::DoesNotExist'
-            proc_output.assertWaitFor(
-                expected_output='Failed to find class with the requested plugin name \'{}\' in the loaded library'.format(component))
-            proc_output.assertWaitFor(
-                expected_output='The component {} was not loaded successfully'.format(component))
-            proc_output.assertWaitFor(
-                expected_output='Aborting the pipeline')
-        else:
-            self.fail('Should abort from component not able to load')
-
-
-    def test_C_run_pkg_not_valid(self, pipeline_manager, proc_info, proc_output):
-        # force third test with 'C'
+    def test_run_pkg_not_valid(self, pipeline_manager, proc_info, proc_output):
         req = ConfigurePipeline.Request()
         pipeline_type = PipelineType()
         test_type = 'example'
@@ -184,7 +114,7 @@ class TestPipelineRun(unittest.TestCase):
                     result = future.result().result
                     break
         if result.output == 1:
-            component = 'example::DoesNotExist'
+            component = 'example::ComponentOne'
             proc_output.assertWaitFor(
                 expected_output='Could not find requested resource in ament index')
             proc_output.assertWaitFor(
