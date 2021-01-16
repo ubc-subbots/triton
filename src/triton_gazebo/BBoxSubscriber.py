9a7365b0bbd49dc90ae2cacfd8774cf2a9ac17b4
Kevin Huang <kevin42@alumni.ubc.ca>
2021-01-16 15:57:00 -0800

    Added Python subscriber to get and save underwater image + bounding box for training

---
+++ src/triton_gazebo/BBoxSubscriber.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+import os
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from triton_interfaces.msg import DetectionBox
+import cv_bridge
+from cv2 import cv2
+import numpy as np
+import random
+
+class BBoxSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('bounding_box_subscriber')
+        self.subscribe_image = self.create_subscription(Image, "/triton/gazebo_drivers/front_camera/underwater/image_raw", self.save_image, 10)
+        self.subscriber_bbox = self.create_subscription(DetectionBox, "/triton/gazebo_drivers/front_camera/bounding_box", self.save_bbox, 10)
+
+    def save_image(self, msg: Image):
+        self.get_logger().info("Getting image...")
+        br = cv_bridge.CvBridge()
+        im = br.imgmsg_to_cv2(msg, "passthrough")
+        self.current_image = im
+
+    def save_bbox(self, msg: DetectionBox):
+        try:
+            rows,cols,channels = self.current_image.shape
+            centre_x = (msg.x + msg.width/2)/cols
+            centre_y = (msg.y + msg.height/2)/rows
+            width = msg.width/cols
+            height = msg.height/rows
+
+            name = "image"+str(random.randint(0,2**16-1))
+            self.get_logger().info("Saving..."+name)
+            txt_string = f"{msg.class_id} {centre_x} {centre_y} {width} {height}"
+            f = open(os.getcwd()+"/src/triton_gazebo/data/" + name + ".txt", "w")
+            f.write(txt_string)
+            f.close()
+            cv2.imwrite(os.getcwd()+"/src/triton_gazebo/data/" + name + ".png", self.current_image)
+        except AttributeError as e:
+            self.get_logger().info("No image yet.")
+            pass
+        
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    subscriber = BBoxSubscriber()
+
+    rclpy.spin(subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/triton_gazebo/launch/gendata_launch.py
@@ -0,0 +1,81 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, EnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import UnlessCondition
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    ld = LaunchDescription()
+
+    world_arg = DeclareLaunchArgument(
+            'world',
+            default_value='uc_gendata.world',
+            description='Gazebo world file'
+    )
+
+    headless_arg = DeclareLaunchArgument(
+            'headless',
+            default_value='true',
+            description="Set to 'true' to run gazebo headless"
+    )
+
+    # We need to add the models and worlds directories to env so gazebo can find them
+    triton_gazebo_dir = get_package_share_directory('triton_gazebo')
+
+    gmp = 'GAZEBO_MODEL_PATH'
+    add_model_path = SetEnvironmentVariable(
+        name=gmp, 
+        value=[
+            EnvironmentVariable(gmp), 
+            os.pathsep + os.path.join(triton_gazebo_dir, 'gazebo', 'models')
+        ]
+    )
+
+    grp = 'GAZEBO_RESOURCE_PATH'
+    add_resource_path = SetEnvironmentVariable(
+        name=grp, 
+        value=[
+            EnvironmentVariable(grp), 
+            os.pathsep + os.path.join(triton_gazebo_dir, 'gazebo')
+        ]
+    )
+
+    gazebo_server = IncludeLaunchDescription(
+        launch_description_source=PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('gazebo_ros'), 
+            'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={
+            'world': ['worlds/', LaunchConfiguration('world')],
+            'verbose': 'true'
+            }.items()
+    )
+
+    gazebo_client = IncludeLaunchDescription(
+        launch_description_source=PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('gazebo_ros'), 
+            'launch', 'gzclient.launch.py')
+        ),
+        condition=UnlessCondition(LaunchConfiguration('headless')),
+    )
+
+    ld.add_action(world_arg)
+    ld.add_action(headless_arg)
+    ld.add_action(add_model_path)
+    ld.add_action(add_resource_path)
+    ld.add_action(gazebo_server)
+    ld.add_action(gazebo_client)
+
+    save_data_subscriber = Node(
+        package="triton_gazebo",
+        executable='BBoxSubscriber.py',
+        name='save_data_subscriber'
+    )
+
+    ld.add_action(save_data_subscriber)
+
+    return ld
