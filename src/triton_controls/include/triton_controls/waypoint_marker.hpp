3ee06acb7ab307705a154968311d585e0436e419
Jared Chan <jaredchan42@gmail.com>
2022-11-01 21:24:01 -0700

    waypoint header

--- src/triton_controls/include/triton_controls/waypoint_marker.hpp
+++ src/triton_controls/include/triton_controls/waypoint_marker.hpp
@@ -4,8 +4,11 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/pose.hpp"
+#include "geometry_msgs/msg/pose_with_covariance.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2/LinearMath/Matrix3x3.h"
+#include "triton_interfaces/msg/waypoint.hpp"
 
 namespace triton_controls
 {
@@ -25,22 +28,34 @@ namespace triton_controls
 
     private:
 
-         /** Pose message callback
+         /** State message callback
          * 
-         * Publishes the waypoint position calculated by taking the waypoint
-         * and adding the initial position given in the first message recieved.
+         * Updates private variable containing the AUV's current pose. 
+         * Determines whether the destination pose is reached if one is set. 
          * 
-         * @param msg geometry pose message witth position and orientation
+         * @param msg geometry pose message with position and orientation
          */
-        void callback(const geometry_msgs::msg::Pose::SharedPtr msg);
+        void state_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
 
-        rclcpp::Publisher<geometry_msgs::msg::Pose>::SharedPtr publisher_;
-        rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr subscription_;
+         /** Waypoint message callback
+         * 
+         * Updates destination waypoint. 
+         * 
+         * @param msg triton_interfaces waypoint message with pose and waypoint type
+         */
+        void waypoint_callback(const triton_interfaces::msg::Waypoint::SharedPtr msg);
 
-        bool has_pose_;
-        geometry_msgs::msg::Pose pose_;
-        std::vector<double> pose_offset_value_;
-        geometry_msgs::msg::Pose pose_offset_;
+        rclcpp::Publisher<triton_interfaces::msg::Waypoint>::SharedPtr publisher_;
+        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr state_subscription_;
+        rclcpp::Subscription<triton_interfaces::msg::Waypoint>::SharedPtr waypoint_subscription_;
+
+        bool waypoint_set_;                         // Whether a waypoint is set
+        geometry_msgs::msg::Pose waypoint_pose_;    // Destination waypoint pose
+        geometry_msgs::msg::Pose current_pose_;     // AUV current pose
+        geometry_msgs::msg::Pose max_pose_offset_;  // Distance criterion
+        float min_stabilize_duration_;              // Time criterion
+        rclcpp::Time last_stable_start_time_;       // Time stamp of the last time meeting waypoint criteria
+        bool waypoint_achieved_;                    // Whether a waypoint is achieved
 
     };
 
--- src/triton_controls/src/waypoint_marker.cpp
+++ src/triton_controls/src/waypoint_marker.cpp
@@ -6,56 +6,46 @@ namespace triton_controls
 
   WaypointMarker::WaypointMarker(const rclcpp::NodeOptions & options)
   : Node("waypoint_marker", options),
-    pose_offset_value_ (6, 0),
-    has_pose_ (false)
+    waypoint_set_ (false),
+    waypoint_achieved_ (false)
   {
-    this->declare_parameter("pos.x", pose_offset_value_[0]);
-    this->declare_parameter("pos.y", pose_offset_value_[1]);
-    this->declare_parameter("pos.z", pose_offset_value_[2]);
-    this->declare_parameter("ang.r", pose_offset_value_[3]);
-    this->declare_parameter("ang.p", pose_offset_value_[4]);
-    this->declare_parameter("ang.y", pose_offset_value_[5]);
 
-    this->get_parameter("pos.x", pose_offset_value_[0]);
-    this->get_parameter("pos.y", pose_offset_value_[1]);
-    this->get_parameter("pos.z", pose_offset_value_[2]);
-    this->get_parameter("ang.r", pose_offset_value_[3]);
-    this->get_parameter("ang.p", pose_offset_value_[4]);
-    this->get_parameter("ang.y", pose_offset_value_[5]);
+    publisher_ = this->create_publisher<triton_interfaces::msg::Waypoint>("/triton/controls/input_pose", 10);
 
-    publisher_ = this->create_publisher<geometry_msgs::msg::Pose>("/triton/controls/input_pose", 10);
+    state_subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
+      "/triton/controls/ukf/odometry/filtered", 10, std::bind(&WaypointMarker::state_callback, this, _1));
 
-    subscription_ = this->create_subscription<geometry_msgs::msg::Pose>(
-      "/triton/state", 10, std::bind(&WaypointMarker::callback, this, _1));
-    RCLCPP_INFO(this->get_logger(), "Waypoint at: x:%f, y:%f, z:%f,  roll:%f, pitch:%f, yaw:%f", pose_offset_value_[0], pose_offset_value_[1], pose_offset_value_[2], pose_offset_value_[3], pose_offset_value_[4], pose_offset_value_[5], pose_offset_value_[6]);
+    waypoint_subscription_ = this->create_subscription<triton_interfaces::msg::Waypoint>(
+      "/triton/controls/waypoint_marker/set", 10, std::bind(&WaypointMarker::waypoint_callback, this, _1));
+
     RCLCPP_INFO(this->get_logger(), "Waypoint Marker successfully started!");
   }
 
 
-  void WaypointMarker::callback(const geometry_msgs::msg::Pose::SharedPtr msg)
+  void WaypointMarker::state_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
   {
-    if (!has_pose_) {
-      has_pose_ = true;
-      pose_ = *msg;
+
+    current_pose_ = msg->pose.pose;
+
+    if (waypoint_set_)
+    {
+      auto reply_msg = triton_interfaces::msg::Waypoint();
+      reply_msg.pose = current_pose_;
+
+      publisher_->publish(reply_msg);
+
     }
-    auto reply_msg = geometry_msgs::msg::Pose();
-    reply_msg = pose_;
-    reply_msg.position.x += pose_offset_value_[0];
-    reply_msg.position.y += pose_offset_value_[1];
-    reply_msg.position.z += pose_offset_value_[2];
-
-    tf2::Quaternion quat;
-    quat.setRPY(pose_offset_value_[3], pose_offset_value_[4], pose_offset_value_[5]);
-
-    reply_msg.orientation.x = quat.x();
-    reply_msg.orientation.y = quat.y();
-    reply_msg.orientation.z = quat.z();
-    reply_msg.orientation.w = quat.w();
-
-    publisher_->publish(reply_msg);
 
   }
 
 
+  void WaypointMarker::waypoint_callback(const triton_interfaces::msg::Waypoint::SharedPtr msg)
+  {
+    auto reply_msg = triton_interfaces::msg::Waypoint();
+    reply_msg.pose = current_pose_;
+
+    publisher_->publish(reply_msg);
+
+  }
 } // namespace triton_controls
 
