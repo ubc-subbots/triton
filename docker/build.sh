9e2b9e768c3594675d5f7afbbc638f502d73a45e
Hashaam Zafar <hashaam1217@gmail.com>
2024-06-04 07:29:38 +0500

    Experimenting with manual installation

--- docker/build.sh
+++ docker/build.sh
@@ -7,7 +7,40 @@ sudo apt update
 # Manual installation (debugging)
 sudo apt install pip -y
 sudo apt update
-sudo apt install libopencv-dev python3-opencv
+
+
+
+### Installing Opencv --START
+#!/bin/bash
+
+# Install prerequisites
+sudo apt update
+sudo apt install -y cmake g++ wget unzip
+
+# Download and unpack OpenCV sources
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.5.3.zip
+unzip opencv.zip
+
+# Create build directory
+mkdir -p build && cd build
+
+# Configure and build OpenCV
+cmake ../opencv-4.5.3
+make -j$(nproc)
+
+# Install OpenCV
+sudo make install
+
+# Clean up
+cd ..
+rm -rf build opencv.zip opencv-4.5.3
+
+### Installing Opencv --END
+
+
+
+
+#sudo apt install libopencv-dev python3-opencv
 # sudo apt install ros-foxy-cv-bridge
 
 # fetch repo
