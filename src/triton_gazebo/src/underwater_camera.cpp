2e042b32d1ddfa7783ac063b369d0d6c2fb753e6
Danial Ramzan <danialramzan@gmail.com>
2024-08-06 22:47:58 -0700

    fix 2

--- src/triton_gazebo/src/underwater_camera.cpp
+++ src/triton_gazebo/src/underwater_camera.cpp
@@ -226,7 +226,11 @@ namespace triton_gazebo
         underwater_image_pub_.publish(message);
 
         auto runtime = this->get_clock()->now() - timer;
-        RCLCPP_INFO(this->get_logger(),"Processing took: "+ std::to_string(runtime.seconds()) + "s");
+
+        // RCLCPP_INFO(this->get_logger(),"Processing took: "+ std::to_string(runtime.seconds()) + "s");
+
+        RCLCPP_INFO(this->get_logger(), "Processing took: %.2fs", runtime.seconds());
+
     }
 
 } // namespace triton_gazebo
